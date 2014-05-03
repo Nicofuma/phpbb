1e34f350dc0979bdc44a03a1a34382b11464012b
Nicofuma <github@nicofuma.fr>
2014-05-03 03:07:15 +0200

    test

--- tests/functional/0000_test.php
+++ tests/functional/0000_test.php
@@ -6,3 +6,13 @@
  * Time: 02:54
  */ 
 phpinfo();
+
+class test extends PHPUnit_Framework_TestCase
+{
+	protected $test_case_helpers;
+
+	public function __construct($name = NULL, array $data = array(), $dataName = '')
+{
+	echo(PHPUnit_Runner_Version::getVersionString());
+}
+}
