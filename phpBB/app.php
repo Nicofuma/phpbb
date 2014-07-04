da26bd1077c1bd040f0acd000e070d10312982ba
Tristan Darricau <github@nicofuma.fr>
2014-07-04 15:49:09 +0200

    Remove the constant in app.php

--- phpBB/app.php
+++ phpBB/app.php
@@ -18,7 +18,6 @@
 * @ignore
 */
 define('IN_PHPBB', true);
-define('PHPBB_IN_APP_PHP', true);
 $phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.' . $phpEx);
