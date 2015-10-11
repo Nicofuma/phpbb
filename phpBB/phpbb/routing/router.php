54460cf37bcb89e7646177b87e72a606ba547743
Matt Friedman <maf675@gmail.com>
2015-10-11 12:09:26 -0700

    Debug

--- phpBB/phpbb/routing/router.php
+++ phpBB/phpbb/routing/router.php
@@ -117,7 +117,7 @@ class router implements RouterInterface
 	 */
 	public function get_routes()
 	{
-		var_dump($this->route_collection->count());
+		var_dump($this->route_collection);
 		if ($this->route_collection === null || $this->route_collection->count() === 0)
 		{
 			$this->route_collection = new RouteCollection;
